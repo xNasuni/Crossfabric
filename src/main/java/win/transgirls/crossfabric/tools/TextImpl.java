f32147f9125b1e8321e5c952b89a6db1460dcf96
Mia <mia@github.com>
2025-10-01 03:27:25 -0400

    fix: rewrite textimpl for more versions

--- src/main/java/win/transgirls/crossfabric/tools/TextImpl.java
+++ src/main/java/win/transgirls/crossfabric/tools/TextImpl.java
@@ -1,63 +1,42 @@
 package win.transgirls.crossfabric.tools;
 
 import win.transgirls.crossfabric.Constants;
-import net.minecraft.text.Text;
 
 import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
+
 import java.util.Objects;
 
-import static win.transgirls.crossfabric.Constants.LOGGER;
+import net.minecraft.text.Text;
 
 public class TextImpl {
-    private static final MethodHandles.Lookup lookup = MethodHandles.lookup();
-
     public static Text translatable(String key, Object... args) {
-        try {
-            Class<?> textClass = ClassUtils.forName("net.minecraft.class_2561", "net.minecraft.text.Text");
-            Class<?> mutableTextClass = ClassUtils.forName("net.minecraft.class_5250", "net.minecraft.text.MutableText");
-            MethodType methodType = MethodType.methodType(mutableTextClass, String.class, Object[].class);
-            MethodHandle translatableMethod = ClassUtils.firstStaticMethodOfName(textClass, methodType, "method_43469", "translatable");
-            return (Text) Objects.requireNonNull(translatableMethod.invoke(key, args));
-        } catch (Throwable e) {
-            try {
-                Class<?> translatableTextClass = ClassUtils.forName("net.minecraft.class_2588", "net.minecraft.text.TranslatableText", "net.minecraft.text.TranslatableTextContent");
-                Constructor<?> translatableTextCtor = translatableTextClass.getConstructor(String.class, Object[].class);
-
-                return (Text) Objects.requireNonNull(translatableTextCtor.newInstance(key, args));
-            } catch (Throwable e2) {
-                LOGGER.error("Couldn't create translatable text for {}", key, e2);
+        try { //----1.16.5->1.19.3
+            Class<?> translatableTextClass = ClassUtils.forName("net.minecraft.network.chat.TranslatableComponent", "net.minecraft.class_2588", "net.minecraft.text.TranslatableText");
+            Constructor<?> ctor = translatableTextClass.getConstructor(String.class, Object[].class);
+            return (Text) Objects.requireNonNull(ctor.newInstance(key, args));
+        } catch (Throwable ignored) {
+            try { //----1.19.4->1.21.9
+                Class<?> translatableTextClass = ClassUtils.forName("net.minecraft.network.chat.TranslatableComponent", "net.minecraft.class_2588", "net.minecraft.text.TranslatableText");
+                Constructor<?> ctor = translatableTextClass.getConstructor(String.class, String.class, Object[].class);
+                return (Text) Objects.requireNonNull(ctor.newInstance(key, key, args));
+            } catch (Throwable ignored2) {
             }
         }
 
-        throw new IllegalStateException(String.format("Text.translatable(key, args) class couldn't be found for %s", Constants.getMinecraftVersion()));
+        throw new IllegalStateException(String.format("Translatable text couldn't be created for %s", Constants.getMinecraftVersion()));
     }
+
     public static Text of(String text) {
-        try {
-            Class<?> textClass = ClassUtils.forName("net.minecraft.class_2561", "net.minecraft.text.Text");
-            Class<?> mutableTextClass = ClassUtils.forName("net.minecraft.class_5250", "net.minecraft.text.MutableText");
-            MethodType methodType = MethodType.methodType(mutableTextClass, String.class);
-            MethodHandle literalMethod = ClassUtils.firstStaticMethodOfName(textClass, methodType, "method_43470", "literal");
-            return (Text) Objects.requireNonNull(literalMethod.invoke(text));
-        } catch (Throwable e) {
-            try {
-                Class<?> textClass = ClassUtils.forName("net.minecraft.class_2561", "net.minecraft.text.Text");
-                MethodType methodType = MethodType.methodType(textClass, String.class);
-                MethodHandle ofMethod = ClassUtils.firstStaticMethodOfName(textClass, methodType, "method_30163", "of");
-                return (Text) Objects.requireNonNull(ofMethod.invoke(text));
-            } catch (Throwable e2) {
-                try {
-                    Class<?> literalTextClass = ClassUtils.forName("net.minecraft.class_8828$class_2585", "net.minecraft.text.LiteralText");
-                    Constructor<?> ctor = literalTextClass.getConstructor(String.class);
-                    return (Text) Objects.requireNonNull(ctor.newInstance(text));
-                } catch (Throwable e3) {
-                    LOGGER.error("Couldn't create literal text for {}", text, e3);
-                }
-            }
+        try { //----1.16.5->1.21.9
+            Class<?> textClass = ClassUtils.forName("net.minecraft.network.chat.Component", "net.minecraft.class_2561", "net.minecraft.text.Text");
+            MethodType methodType = MethodType.methodType(textClass, String.class);
+            MethodHandle ofMethod = ClassUtils.firstStaticMethodOfName(textClass, methodType, "nullToEmpty", "method_30163", "of");
+            return (Text) Objects.requireNonNull(ofMethod.invoke(text));
+        } catch (Throwable ignored){
         }
 
-        throw new IllegalStateException(String.format("Text.of class couldn't be found for %s", Constants.getMinecraftVersion()));
+        throw new IllegalStateException(String.format("Literal text couldn't be created for %s", Constants.getMinecraftVersion()));
     }
 }
